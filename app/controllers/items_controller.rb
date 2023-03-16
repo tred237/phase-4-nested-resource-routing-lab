99388b02805964c634b037458d97bf8a26d11842
trevor dewindt <trevor.dewindt@gmail.com>
2023-03-16 17:25:09 -0600

    commit tree 205bc5e8a62b3c2747e716fb9af50aa0a2dd60c7

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
 
   def index
-    items = Item.all
+    items = params[:user_id] ? find_user.items : Item.all
     render json: items, include: :user
+  end
+
+  def show
+    item_id = params[:id]
+    item = params[:user_id] ? find_user.items.find(item_id) : Item.find(item_id)
+    render json: item, include: :user
+  end
+
+  def create
+    item = find_user.items.create(items_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def items_params
+    params.permit(:name, :description, :price)
+  end
+
+  def record_not_found_response
+    render json: {error: "Record Not Found"}, status: :not_found
   end
 
 end
